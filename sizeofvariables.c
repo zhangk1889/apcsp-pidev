c7b61ec6dd5fa0fa12c81eb5359e635e8acd75f4
Kevin <zhangk1889@my.sduhsd.net>
2019-12-19 12:53:58 -0800

    Sizes

---
+++ sizeofvariables.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+  int a = 545;
+  int b = 600;
+  char c = 125;
+  float d = 123456789.123456789;
+
+  // print value and size of an int variable
+  ;printf("int a value: %d and size: %d bytes\n", a, sizeof(a));
+  ;printf("int b value: %d and size: %d bytes\n", b, sizeof(b));
+  ;printf("int c value: %d and size: %d bytes\n", c, sizeof(c));
+  ;printf("int d value: %d and size: %d bytes\n", d, sizeof(d));
+}
+
+
