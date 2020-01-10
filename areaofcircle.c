557d26ef4eeb79430c478076a58605b757664dc8
zhangk1889 <58750927+zhangk1889@users.noreply.github.com>
2020-01-10 12:35:14 -0800

    Update areaofcircle.c

--- areaofcircle.c
+++ areaofcircle.c
@@ -2,14 +2,16 @@
 
 float b;
 float area (float r);
-int main(){
-for(float r=3.5; r< 12.6;r++){
+
+int main()
+ {
+ for(float r=3.5; r< 12.6;r++){
 b = area(r);
 printf("Value b - %f\n", b);
 }
 }
-
 float area(float r)
+
 {
 float a;
 a = r * r * 3.14;
